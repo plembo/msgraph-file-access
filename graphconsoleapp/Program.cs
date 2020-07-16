b8ad0c79810c506f390e4ebf8a4432e07cb6a1ab
Phil Lembo <phil.lembo@gmail.com>
2020-07-16 09:30:17 -0400

    Add LoadAppSettings class

--- graphconsoleapp/Program.cs
+++ graphconsoleapp/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using Microsoft.Extensions.Configuration;
+using Helpers;
 
 namespace graphconsoleapp
 {
@@ -9,4 +16,28 @@ namespace graphconsoleapp
             Console.WriteLine("Hello World!");
         }
     }
+
+    private static IConfigurationRoot LoadAppSettings()
+    {
+    try
+    {
+        var config = new ConfigurationBuilder()
+                        .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", false, true)
+                        .Build();
+
+        if (string.IsNullOrEmpty(config["applicationId"]) ||
+            string.IsNullOrEmpty(config["tenantId"]))
+        {
+            return null;
+        }
+
+        return config;
+    }
+        catch (System.IO.FileNotFoundException)
+        {
+            return null;
+        }   
+    }
+
 }
