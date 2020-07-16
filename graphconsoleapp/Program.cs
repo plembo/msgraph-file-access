bf1e63f87d628ca3735e02d0469454f34cdc425a
Phil Lembo <phil.lembo@gmail.com>
2020-07-16 12:58:22 -0400

    Whitespace

--- graphconsoleapp/Program.cs
+++ graphconsoleapp/Program.cs
@@ -44,6 +44,7 @@ namespace graphconsoleapp
             var results = request.GetAsync().Result;
             Console.WriteLine(results.Id + ": " + results.Name);
             */
+            
             // request 3 - download specific file
             // var fileId = "REPLACE_THIS";
             var request = client.Me.Drive.Items[fileId].Content.Request();
